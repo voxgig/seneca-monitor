41545aa5f6d37cb979017528dd2f875e516048b4
Richard Rodger <richard@ricebridge.com>
2017-08-10 13:42:21 +0100

    fixes for seneca 3.4.2

--- monitor.js
+++ monitor.js
@@ -9,22 +9,25 @@ var Inert = require('inert')
 module.exports = function monitor(options) {
   var seneca = this
 
-  var opts = this.util.deepextend({
-    udp: {
-      kind: 'udp4',
-      port: 10111,
-      host: '0.0.0.0'
+  var opts = this.util.deepextend(
+    {
+      udp: {
+        kind: 'udp4',
+        port: 10111,
+        host: '0.0.0.0'
+      },
+      web: {
+        port: 10181,
+        host: '0.0.0.0',
+        folder: __dirname
+      }
     },
-    web: {
-      port: 10181,
-      host: '0.0.0.0',
-      folder: __dirname
-    }
-  },options)
+    options
+  )
 
   var ds = Dgram.createSocket(opts.udp.kind)
 
-  var spec = { ds: ds, seneca: seneca, opts:opts }
+  var spec = { ds: ds, seneca: seneca, opts: opts }
 
   if (options.collect) {
     return make_collector(spec)
@@ -34,8 +37,8 @@ module.exports = function monitor(options) {
 }
 
 function make_monitor(spec) {
-  spec.seneca.on('act-in', function(msg) {
-    var m = msg.meta$
+  spec.seneca.on('act-in', function(msg, ignore, meta) {
+    var m = msg.meta$ || meta
 
     var parents = m.parents || []
     var client = parents[0]
@@ -43,8 +46,6 @@ function make_monitor(spec) {
     if (!client) {
       return
     }
-
-    // console.log(m)
 
     var desc = [
       'M',
@@ -65,31 +66,37 @@ function make_monitor(spec) {
     send(spec, desc)
   })
 
-  spec.seneca.add('role:seneca,cmd:close', function (msg, reply) {
+  spec.seneca.add('role:seneca,cmd:close', function(msg, reply) {
     var seneca = this
-    //console.log('CLOSE')
-    send(spec,'D~'+spec.seneca.id)
-    setTimeout(function() {
-      spec.ds.close(function () {
-        spec.ds = null
-        seneca.prior(msg,reply)
-      })
-    },100)
-  })
 
+    send(spec, 'D~' + spec.seneca.id)
+    setTimeout(function() {
+      spec.ds.close(function() {
+        spec.ds = null
+        seneca.prior(msg, reply)
+      })
+    }, 100)
+  })
 }
 
 function send(spec, desc) {
-  if (!spec.ds) return;
+  if (!spec.ds) return
 
   var data = new Buffer(desc)
-  spec.ds.send(data, 0, data.length, spec.opts.udp.port, spec.opts.udp.host, function(err) {
-    if (err) spec.seneca.log.warn(err)
-  })
+  spec.ds.send(
+    data,
+    0,
+    data.length,
+    spec.opts.udp.port,
+    spec.opts.udp.host,
+    function(err) {
+      if (err) spec.seneca.log.warn(err)
+    }
+  )
 }
 
 function make_collector(spec) {
-  if (!spec.ds) return;
+  if (!spec.ds) return
 
   spec.ds.on('message', function(data) {
     update(data.toString().split('~'))
@@ -99,40 +106,39 @@ function make_collector(spec) {
   var map = {}
   spec.map = map
 
-  spec.seneca.add('role:monitor,get:map', function (msg, reply) {
+  spec.seneca.add('role:monitor,get:map', function(msg, reply) {
     reply(map)
   })
 
-  spec.seneca.add('role:monitor,cmd:update', function (msg, reply) {
-    update(msg.data||[])
+  spec.seneca.add('role:monitor,cmd:update', function(msg, reply) {
+    update(msg.data || [])
     reply()
   })
 
   // TODO: should be plugin init
   make_web(spec, console.log)
 
-  
   function update(data) {
     var cmd = data[0]
 
-    if( 'D' == cmd ) {
+    if ('D' == cmd) {
       var did = data[1]
 
-      Object.keys(map).forEach(function (kid) {
-        if(kid === did) {
+      Object.keys(map).forEach(function(kid) {
+        if (kid === did) {
           delete map[did]
           return
         }
-        Object.keys(map[kid].in).forEach(function (pat) {
-          Object.keys(map[kid].in[pat]).forEach(function (mid) {
-            if(mid === did) {
+        Object.keys(map[kid].in).forEach(function(pat) {
+          Object.keys(map[kid].in[pat]).forEach(function(mid) {
+            if (mid === did) {
               delete map[kid].in[pat][did]
             }
           })
         })
-        Object.keys(map[kid].out).forEach(function (pat) {
-          Object.keys(map[kid].out[pat]).forEach(function (mid) {
-            if(mid === did) {
+        Object.keys(map[kid].out).forEach(function(pat) {
+          Object.keys(map[kid].out[pat]).forEach(function(mid) {
+            if (mid === did) {
               delete map[kid].out[pat][did]
             }
           })
@@ -143,27 +149,26 @@ function make_collector(spec) {
 
     var o = 1
 
-    var pattern = data[o+0]
+    var pattern = data[o + 0]
 
     if (-1 != pattern.indexOf('role:seneca')) {
       return
     }
 
-    var sync = data[o+5]
-    var start = data[o+6]
+    var sync = data[o + 5]
+    var start = data[o + 6]
 
-    var rid = data[o+2]
-    var rtag = data[o+3]
-    var rver = data[o+4]
+    var rid = data[o + 2]
+    var rtag = data[o + 3]
+    var rver = data[o + 4]
 
-    var sid = data[o+9]
-    var stag = data[o+10]
-    var sver = data[o+11]
+    var sid = data[o + 9]
+    var stag = data[o + 10]
+    var sver = data[o + 11]
 
-    if( '' === pattern || rid === sid ) {
+    if ('' === pattern || rid === sid) {
       return
     }
-
 
     /*
     console.log(
@@ -179,11 +184,10 @@ function make_collector(spec) {
     )
      */
 
+    var r = (map[rid] = map[rid] || { in: {}, out: {}, tag: rtag })
+    var s = (map[sid] = map[sid] || { in: {}, out: {}, tag: stag })
 
-    var r = (map[rid] = map[rid] || {in:{},out:{},tag:rtag}) 
-    var s = (map[sid] = map[sid] || {in:{},out:{},tag:stag}) 
-
-    var rin = r.in[pattern] = (r.in[pattern] || {})
+    var rin = (r.in[pattern] = r.in[pattern] || {})
     var sd = (rin[sid] = rin[sid] || {})
     sd.t = stag
     sd.v = sver
@@ -191,7 +195,7 @@ function make_collector(spec) {
     sd.c = 1 + (sd.c || 0)
     sd.w = start
 
-    var sin = s.out[pattern] = (s.out[pattern] || {})
+    var sin = (s.out[pattern] = s.out[pattern] || {})
     var rd = (sin[rid] = sin[rid] || {})
     rd.t = rtag
     rd.v = rver
@@ -203,31 +207,31 @@ function make_collector(spec) {
   }
 }
 
-
 function make_web(spec, done) {
   var server = new Hapi.Server()
-  server.connection({port: spec.opts.web.port, host:spec.opts.web.host})
+  server.connection({ port: spec.opts.web.port, host: spec.opts.web.host })
 
-  server.register( Inert )
+  server.register(Inert)
 
   server.route({
     method: 'GET',
     path: '/{path*}',
     handler: {
       directory: {
-        path: spec.opts.web.folder + '/www',
+        path: spec.opts.web.folder + '/www'
       }
     }
   })
 
-  server.route({ 
-    method: 'GET', path: '/api/map', 
-    handler: function( request, reply ) {
-      spec.seneca.act('role:monitor,get:map', function (err, out) {
-        reply(err||out||{})
+  server.route({
+    method: 'GET',
+    path: '/api/map',
+    handler: function(request, reply) {
+      spec.seneca.act('role:monitor,get:map', function(err, out) {
+        reply(err || out || {})
       })
     }
   })
 
-  server.start(done)      
+  server.start(done)
 }
--- test/monitor.test.js
+++ test/monitor.test.js
@@ -17,48 +17,50 @@ describe('monitor', function() {
   })
 
   it('happy', function(fin) {
-    var collect = Seneca({id$:'M0', tag:'m0'}).test(fin).use('..', { collect: true })
+    var collect = Seneca({ id$: 'M0', tag: 'm0' })
+      .test(fin)
+      .use('..', { collect: true })
 
     var xI = 0
 
-    var t0 = Seneca({ id$:'T0', tag: 't0', version: '1.0.0' })
-          .test(fin)
-          .use('..')
-          .add('t:3')
-          .listen(20000)
+    var t0 = Seneca({ id$: 'T0', tag: 't0', version: '1.0.0' })
+      .test(fin)
+      .use('..')
+      .add('t:3')
+      .listen(20000)
 
-    var t1 = Seneca({ id$:'T1', tag: 't0', version: '1.0.0' })
-          .test(fin)
-          .use('..')
-          .add('t:3')
-          .listen(20001)
+    var t1 = Seneca({ id$: 'T1', tag: 't0', version: '1.0.0' })
+      .test(fin)
+      .use('..')
+      .add('t:3')
+      .listen(20001)
 
-    var s0 = Seneca({ id$:'S0', tag: 's0', version: '1.0.0' })
-          .test(fin)
-          .use('..')
-          .add('a:1')
-          .add('b:2')
-          .add('c:3', function (msg, reply) {
-            this.act('t:3',{x:(xI++)%2}, reply)
-          })
-          .listen(20100)
-          .client({pin:'t:3,x:0', port:20000})
-          .client({pin:'t:3,x:1', port:20001})
+    var s0 = Seneca({ id$: 'S0', tag: 's0', version: '1.0.0' })
+      .test(fin)
+      .use('..')
+      .add('a:1')
+      .add('b:2')
+      .add('c:3', function(msg, reply) {
+        this.act('t:3', { x: xI++ % 2 }, reply)
+      })
+      .listen(20100)
+      .client({ pin: 't:3,x:0', port: 20000 })
+      .client({ pin: 't:3,x:1', port: 20001 })
 
-    var c1 = Seneca({ id$:'C1', tag: 'c1', version: '2.0.0' })
-          .test(fin)
-          .use('..')
-          .client({pins:['a:1','b:2','c:3'], port:20100})
+    var c1 = Seneca({ id$: 'C1', tag: 'c1', version: '2.0.0' })
+      .test(fin)
+      .use('..')
+      .client({ pins: ['a:1', 'b:2', 'c:3'], port: 20100 })
 
-    var c2 = Seneca({ id$:'C2', tag: 'ca', version: '3.0.0' })
-          .test(fin)
-          .use('..')
-          .client({pins:['a:1','b:2','c:3'], port:20100})
+    var c2 = Seneca({ id$: 'C2', tag: 'ca', version: '3.0.0' })
+      .test(fin)
+      .use('..')
+      .client({ pins: ['a:1', 'b:2', 'c:3'], port: 20100 })
 
-    var c3 = Seneca({ id$:'C3', tag: 'ca', version: '3.0.1' })
-          .test(fin)
-          .use('..')
-          .client({pins:['a:1','b:2','c:3'], port:20100})
+    var c3 = Seneca({ id$: 'C3', tag: 'ca', version: '3.0.1' })
+      .test(fin)
+      .use('..')
+      .client({ pins: ['a:1', 'b:2', 'c:3'], port: 20100 })
 
     console.log()
 
@@ -74,11 +76,10 @@ describe('monitor', function() {
 
       setTimeout(function() {
         collect.act('role:monitor,get:map', function(err, out) {
-
-          console.dir(out,{depth:null,colors:true})
+          console.dir(out, { depth: null, colors: true })
           fin()
         })
-      },100)
-    },200)
+      }, 100)
+    }, 200)
   })
 })
